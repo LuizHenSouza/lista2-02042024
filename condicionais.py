5a8f9e3a0547b4e25fd847136aac350854cd7ee4
Luiz Souza <LuizCraftPB@Gmail.com>
2024-04-02 20:56:23 -0300

    feat_verifica_par_ou_impar_add

--- condicionais.py
+++ condicionais.py
@@ -9,5 +9,17 @@ def verifica_maior_idade():
         print("Valor inserido é invaldo!")
         verifica_maior_idade()
 
-
 verifica_maior_idade()
+
+def verifica_par_ou_impar():
+    try:
+        valor = int(input("Digite um número:"))
+        if(valor%2 == 0):
+            print("O número é par")
+        else:
+            print("O número é ímpar")
+    except:
+        print("valor inserido é invalido")
+        verifica_par_ou_impar()
+
+verifica_par_ou_impar()
